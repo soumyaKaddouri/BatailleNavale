8aa68d4952fe7e5b21de520465f77dd64445ff53
yaya752 <yassine76440@gmail.com>
2023-03-10 15:31:28 +0100

    vrai position x y

--- NavalWar.API/Controllers/PlayerController.cs
+++ NavalWar.API/Controllers/PlayerController.cs
@@ -69,8 +69,79 @@ namespace NavalWar.API.Controllers
         public ActionResult Shoot(int id, int x, int y)
         {
             int resultat;
-            x = x + 4;
-            y = y - 4;
+            if (x == -4)
+            {
+                x = 0;
+            }
+            if (x == -3)
+            {
+                x = 1;
+            }
+            if (x == -2)
+            {
+                x = 2;
+            }
+            if (x == -1)
+            {
+                x = 3;
+            }
+            if (x == 0)
+            {
+                x = 4;
+            }
+            if (x == 1)
+            {
+                x = 5;
+            }
+            if (x == 2)
+            {
+                x = 6;
+            }
+            if (x == 3)
+            {
+                x = 7;
+            }
+            if (x == 4)
+            {
+                x = 8;
+            }
+            if (y == -4)
+            {
+                y = 8;
+            }
+            if (y == -3)
+            {
+                y = 7;
+            }
+            if (y == -2)
+            {
+                y = 6;
+            }
+            if (y == -1)
+            {
+                y = 5;
+            }
+            if (y == 0)
+            {
+                y = 4;
+            }
+            if (y == 1)
+            {
+                y = 3;
+            }
+            if (y == 2)
+            {
+                y = 2;
+            }
+            if (y == 3)
+            {
+                y = 1;
+            }
+            if (y == 4)
+            {
+                y = 0;
+            }
+
             var player = _player.GetPlayerById(id);
             var session = _sess.GetSessionById(player.IdSession);
             if (session.GameState == 1)
